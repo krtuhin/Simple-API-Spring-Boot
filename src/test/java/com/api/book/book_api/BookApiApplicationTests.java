8dbe0f472699beb632c9c230316898c36cd2ecea
krtuhin <tuhin.km2774@gmail.com>
2023-07-30 02:15:16 +0530

    Project Created

---
+++ src/test/java/com/api/book/book_api/BookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.book.book_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
