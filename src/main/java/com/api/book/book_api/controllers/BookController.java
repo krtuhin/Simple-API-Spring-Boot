d3558ad507ac5b9ebdf172f2de2dfad2c713a411
krtuhin <tuhin.km2774@gmail.com>
2023-07-30 11:57:26 +0530

    Controller with RestController & GetMapping annotation (retrieve data using get method)

--- src/main/java/com/api/book/book_api/controllers/BookController.java
+++ src/main/java/com/api/book/book_api/controllers/BookController.java
@@ -1,7 +1,14 @@
 package com.api.book.book_api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.book.book_api.entities.Book;
+import com.api.book.book_api.services.BookService;
 
 //@RestController - used in rest api in place of @Controller
 //when use @RestController, don't need @ResponseBody annotation
@@ -10,8 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
+    // method for fetch all books
     @GetMapping("/books")
-    public String getBooks() {
-        return "Testing first time";
+    public List<Book> getBooks() {
+        return this.bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") int id) {
+        return this.bookService.getBook(id);
     }
 }
